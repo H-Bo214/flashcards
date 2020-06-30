193f135a1d19d46e7ffd3d3670d4a9d05d7926da
Horacio Borrego <hborrego365@gmail.com>
2020-06-30 17:20:19 -0600

    Add Round class, properties of deck, turnCount, incorrectGuesses, method returnCurrentCard, method takeTurn, method calculatePercentCorrect, method endRound and test for all of these

---
+++ src/Round.js
@@ -0,0 +1,39 @@
+const Turn = require('../src/Turn');
+
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.turnCount = 0;
+    this.incorrectGuesses = [];
+  };
+
+  returnCurrentCard() {
+    return this.deck.deck[this.turnCount]
+  };
+
+  takeTurn(guess) {
+    const turn = new Turn(guess, this.returnCurrentCard())
+    if (turn.evaluateGuess() === false) {
+      this.incorrectGuesses.push(turn.card.id)
+    }
+    this.turnCount++;
+    return turn.giveFeedback()
+  };
+
+  calculatePercentCorrect() {
+    if (this.incorrectGuesses.length === 0) {
+      return 100;
+    } else {
+      let percentage = Math.floor(this.incorrectGuesses.length / this.turnCount * 100)
+      return percentage;
+    }
+  };
+
+  endRound() {
+    console.log(`**Round over!** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
+    return `**Round over!** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+  };
+
+};
+
+module.exports = Round;
